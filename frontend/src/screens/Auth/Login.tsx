f397df7d19046f4345c4a20d4dbefe5861350ae1
MATHEUS HENRIQUE SCHOPP PEIXOTO <0031432412024@lti.fatecso.edu.br>
2025-09-10 15:42:49 -0300

    fix: alert removed

--- frontend/src/screens/Auth/Login.tsx
+++ frontend/src/screens/Auth/Login.tsx
@@ -31,7 +31,7 @@ export const Login = () => {
       navigate("/dashboard");
     } catch (err: any) {
       console.error(err);
-      toast.error(err?.response?.data?.mensagem || "Falha no login. Verifique suas credenciais.");
+      toast.error(err?.response?.data?.mensagem || "Falha ao entrar");
     }
   };
 
